83d296872df818da0d9f591e304062b71e43daff
Vaishnavi Sarsodiya <sarsodiya2405@gmail.com>
2025-08-02 04:22:32 -0700

    added mutiple files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+    <Header></Header>
+    </>
+      )
+      }
+      export default App;
